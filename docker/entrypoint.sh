c8cf06d7a5af799648b21b4e515acf0048c54f86
Lars Gersmann <lars.gersmann@cm4all.com>
2022-11-22 13:48:57 +0100

    fix(docker): error message will be sent to stderr

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,13 +4,13 @@ set -e
 
 # ensure stdin is provided using docker -i argument
 if ls -Ll /proc/self/fd/0 | grep -q ' 1,  *3 '; then
-  echo "container has no access to stdin. Run 'docker run -i ...' to run the container with stdin enabled"
+  echo "container has no access to stdin. Run 'docker run -i ...' to run the container with stdin enabled" 1>&2
   exit 1
 fi
 
 # ensure mount point /app bound to host repo directory using docker --mount argument
 if [[ ! -d /app ]]; then
-  echo "mount point '/app' is not bound to a host directory. Run 'docker ... --mount type=bind,source=<host-repo-directory>,target=/app ...'"
+  echo "mount point '/app' is not bound to a host directory. Run 'docker ... --mount type=bind,source=<host-repo-directory>,target=/app ...'" 1>&2
   exit 1
 fi 
 
